b2b430e7179925edff97d1fcde5dc044cf89a8ec
Jaime <jaimeloeuf@gmail.com>
2024-04-21 18:09:49 +0800

    Move layout's HTML template out into index.html:     To make it easier to write HTML and styles with editor support     instead of doing it in the template string.

--- layout/index.ts
+++ layout/index.ts
@@ -1,15 +1,13 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+const htmlTemplate = readFileSync(
+  // Path should be relative to /tool/
+  resolve("../layout/index.html"),
+  { encoding: "utf8" }
+);
+
 export const generateHtml = (title: string, postContent: string) =>
-  `<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${title}</title>
-</head>
-
-<body>
-    ${postContent}
-</body>
-
-</html>`;
+  htmlTemplate
+    .replace("${title}", title)
+    .replace("${postContent}", postContent);
