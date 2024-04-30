199849d8182ead45ea4164e96fa7dc2655899c9e
Jaime <jaimeloeuf@gmail.com>
2024-04-30 21:12:27 +0800

    Split buildTags function up:     To split up the all tags page and individual tag page build process.

--- tool/src/buildTags.ts
+++ tool/src/buildTags.ts
@@ -7,6 +7,47 @@ import { generateHomePostCardHtml } from "./layout/components/homePostCard";
 import { createFolderIfDoesNotExist } from "./createFolderIfDoesNotExist";
 import type { Post } from "./Post";
 
+type Tags = Map<string, { rawTag: string; count: number }>;
+
+async function buildAllTags(tagsFolderPath: string, tags: Tags) {
+  const tagCardHTML = new Array(...tags)
+    .sort(([a], [b]) => a.localeCompare(b))
+    .map(([tag, { rawTag, count }]) =>
+      generateHomeTagCardHtml(tag, rawTag, count),
+    )
+    .join("");
+
+  const allTagHTML = generateAllTagsHtml(tagCardHTML);
+  await writeFile(path.resolve(tagsFolderPath, `index.html`), allTagHTML, {
+    flag: "w",
+  });
+}
+
+async function buildIndividualTagPage(
+  tagsFolderPath: string,
+  posts: Array<Post>,
+  tags: Tags,
+) {
+  // Generate a page for every single tag, to show all posts with that tag
+  for (const [tag, { rawTag, count }] of tags) {
+    const postCardHTML = posts
+      .filter((post) => post.tags.some((postTag) => postTag.tag === tag))
+      .map((post) =>
+        generateHomePostCardHtml(
+          post.folderName,
+          post.title,
+          post.date.toDateString(),
+        ),
+      )
+      .join("");
+
+    const tagHTML = generateTagsHtml(rawTag, count, postCardHTML);
+
+    const htmlFilePath = path.resolve(tagsFolderPath, `${tag}.html`);
+    await writeFile(htmlFilePath, tagHTML, { flag: "w" });
+  }
+}
+
 export async function buildTags(
   buildOutputFolderPath: string,
   posts: Array<Post>,
@@ -23,34 +64,6 @@ export async function buildTags(
       return map.set(tag, tagObject);
     }, new Map<string, { rawTag: string; count: number }>());
 
-  const tagCardHTML = new Array(...tags)
-    .sort(([a], [b]) => a.localeCompare(b))
-    .map(([tag, { rawTag, count }]) =>
-      generateHomeTagCardHtml(tag, rawTag, count),
-    )
-    .join("");
-
-  const allTagHTML = generateAllTagsHtml(tagCardHTML);
-  await writeFile(path.resolve(tagsFolderPath, `index.html`), allTagHTML, {
-    flag: "w",
-  });
-
-  // Generate a page for every single tag, to show all posts with that tag
-  for (const [tag, { rawTag, count }] of tags) {
-    const postCardHTML = posts
-      .filter((post) => post.tags.some(({ tag: TAG }) => TAG === tag))
-      .map((post) =>
-        generateHomePostCardHtml(
-          post.folderName,
-          post.title,
-          post.date.toDateString(),
-        ),
-      )
-      .join("");
-
-    const tagHTML = generateTagsHtml(rawTag, count, postCardHTML);
-
-    const htmlFilePath = path.resolve(tagsFolderPath, `${tag}.html`);
-    await writeFile(htmlFilePath, tagHTML, { flag: "w" });
-  }
+  await buildAllTags(tagsFolderPath, tags);
+  await buildIndividualTagPage(tagsFolderPath, posts, tags);
 }
