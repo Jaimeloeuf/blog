ff37bb516e23478f9180fccc764608a74a4c5dfe
Jaime <jaimeloeuf@gmail.com>
2024-05-12 22:50:20 +0800

    Wrap nodemon watcher in function:     To make it easier to comment out when testing other implementations.

--- tool/serve.js
+++ tool/serve.js
@@ -1,9 +1,13 @@
-const nodemon = require("nodemon");
+function nodemonWatcher() {
+  const nodemon = require("nodemon");
 
-nodemon({
-  exec: "npm run build",
-  ext: "ts md",
-  watch: ["./", "../posts/**/*"],
-})
-  .on("start", () => console.log("Starting..."))
-  .on("restart", () => console.log("Rebuild..."));
+  nodemon({
+    exec: "npm run build",
+    ext: "ts md",
+    watch: ["./", "../posts/**/*"],
+  })
+    .on("start", () => console.log("Starting..."))
+    .on("restart", () => console.log("Rebuild..."));
+}
+
+nodemonWatcher();
