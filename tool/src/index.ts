128aec50e3ad57249a6d843409ea0fa7cf5c6651
Jaime <jaimeloeuf@gmail.com>
2024-05-14 00:02:31 +0800

    Refactor main build function out:     So that it can be imported and called elsewhere without running it     directly / or called to run repeatedly.

--- tool/src/index.ts
+++ tool/src/index.ts
@@ -1,43 +1,11 @@
-import { readdir } from "fs/promises";
-import { createBuildOutputFolder } from "./utils/createBuildOutputFolder";
-import { postsDirPath } from "./utils/postsDirPath";
-import { buildStyleSheet } from "./buildStyleSheet";
-import { buildPosts } from "./buildPosts";
-import { buildTags } from "./buildTags";
-import { buildHomePage } from "./buildHomePage";
-import { buildTagPages } from "./buildTagPages";
-import { buildNotFoundPage } from "./buildNotFoundPage";
-import { buildAssets } from "./buildAssets";
-import { deleteRemovedFilesInOutputFolder } from "./deleteRemovedFilesInOutputFolder";
+import { build } from "./build";
 
-async function main() {
+async function bootStrap() {
   console.time("Build time");
 
-  const buildOutputFolderPath = await createBuildOutputFolder();
-  const postFolderItems = await readdir(postsDirPath);
-
-  const posts = await buildPosts(buildOutputFolderPath, postFolderItems);
-  const tags = buildTags(posts);
-
-  const homePagePath = await buildHomePage(buildOutputFolderPath, posts, tags);
-  const tagPagePaths = await buildTagPages(buildOutputFolderPath, posts, tags);
-  const notFoundPagePath = await buildNotFoundPage(buildOutputFolderPath);
-  const assetFilePaths = await buildAssets(buildOutputFolderPath);
-  const styleSheetPath = await buildStyleSheet(buildOutputFolderPath);
-
-  await deleteRemovedFilesInOutputFolder(
-    buildOutputFolderPath,
-
-    /* All the valid paths to keep */
-    homePagePath,
-    notFoundPagePath,
-    styleSheetPath,
-    ...tagPagePaths,
-    ...assetFilePaths,
-    ...posts.map((post) => post.outputPaths).flat(),
-  );
+  await build();
 
   console.timeEnd("Build time");
 }
 
-main();
+bootStrap();
