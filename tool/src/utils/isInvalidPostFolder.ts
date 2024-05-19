a4c0660337f6d6c7d050c62b9eb6ffbb6d57dfd2
Jaime <jaimeloeuf@gmail.com>
2024-05-19 16:46:09 +0800

    Add check for fs stat call.

--- tool/src/utils/isInvalidPostFolder.ts
+++ tool/src/utils/isInvalidPostFolder.ts
@@ -7,7 +7,14 @@ import { postsDirPath } from "./postsDirPath";
  * invalid, by treating special folders and standalone files as invalid.
  */
 export async function isInvalidPostFolder(postFolderItemName: string) {
-  const itemStat = await stat(path.resolve(postsDirPath, postFolderItemName));
+  const itemStat = await stat(path.resolve(postsDirPath, postFolderItemName))
+    .then((itemStat) => itemStat)
+    .catch(() => new Error(`Failed to read stat of: ${postFolderItemName}`));
+
+  if (itemStat instanceof Error) {
+    console.error(itemStat);
+    return true;
+  }
 
   if (postFolderItemName.startsWith("__") || itemStat.isFile()) {
     return true;
