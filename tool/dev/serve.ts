0d70b437be61a5027662b146a6bd9fe710c4eb1e
Jaime <jaimeloeuf@gmail.com>
2024-05-26 02:38:55 +0800

    Optimise watcher's static asset change rebuilds.

--- tool/dev/serve.ts
+++ tool/dev/serve.ts
@@ -5,7 +5,12 @@ import { logger } from "../shared/logger";
 import { startDevServer } from "./devServer";
 import { build } from "../src/build";
 import { buildPost } from "../src/buildPost";
-import { postsDirPath, templateDirPath } from "../src/utils/dirPaths";
+import { buildAssets } from "../src/buildAssets";
+import {
+  postsDirPath,
+  templateDirPath,
+  assetsDirPath,
+} from "../src/utils/dirPaths";
 import { isInvalidPostFolder } from "../src/utils/isInvalidPostFolder";
 import { fullRebuildOnToolChange } from "./fullRebuildOnToolChange";
 import { removeFileFromRfsCache } from "../src/utils/rfs";
@@ -83,6 +88,19 @@ async function chokidarWatcher() {
 
         removeFileFromRfsCache(templateFileName);
         build();
+
+        return;
+      }
+
+      if (path.includes("/assets/")) {
+        const assetFileName = relative(assetsDirPath, path);
+
+        logger.verbose(
+          `chokidarWatcher:tool-change:assets`,
+          `Static asset '${assetFileName}' changed, rebuilding assets only`,
+        );
+
+        buildAssets(buildOutputFolderPath);
 
         return;
       }
