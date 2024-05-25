f7498bedf4da33f3aff914e6a371ed6924075754
Jaime <jaimeloeuf@gmail.com>
2024-05-26 01:49:08 +0800

    Refactor out assets dir path.

--- tool/src/buildAssets.ts
+++ tool/src/buildAssets.ts
@@ -1,14 +1,13 @@
 import path from "path";
 import { readdir, copyFile } from "fs/promises";
+import { assetsDirPath } from "./utils/assetsDirPath";
 
 /**
  * Build static assets content by copying them into build output folder.
  */
 export async function buildAssets(buildOutputFolderPath: string) {
-  const assetsFolderPath = path.resolve("./src/assets");
-
   // Copy over all other supporting static assets.
-  const folderContents = await readdir(assetsFolderPath, {
+  const folderContents = await readdir(assetsDirPath, {
     encoding: "utf8",
   });
 
@@ -17,7 +16,7 @@ export async function buildAssets(buildOutputFolderPath: string) {
   for (const folderContent of folderContents) {
     const outputPath = path.resolve(buildOutputFolderPath, folderContent);
 
-    await copyFile(path.resolve(assetsFolderPath, folderContent), outputPath);
+    await copyFile(path.resolve(assetsDirPath, folderContent), outputPath);
 
     assetFilePaths.push(outputPath);
   }
---
+++ tool/src/utils/assetsDirPath.ts
@@ -0,0 +1,3 @@
+import path from "path";
+
+export const assetsDirPath = path.resolve("./src/assets");
