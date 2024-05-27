edffd219924b7fcf5d83cc11fdc8190bede9d60e
Jaime <jaimeloeuf@gmail.com>
2024-05-27 12:39:48 +0800

    Implement subscribe feature using muwno API.

--- tool/src/assets/subscribe.js
+++ tool/src/assets/subscribe.js
@@ -1,3 +1,32 @@
+import { sf } from "https://cdn.jsdelivr.net/npm/simpler-fetch@10.4.0/+esm";
+
+// Initialise this asynchronously and only for the pages that load subscribeModal.
+(function initialiseRecaptcha() {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src =
+    "https://www.recaptcha.net/recaptcha/api.js?render=6Ldwb4InAAAAAP8Z6U1JnGq3WQ9pguOtWGh3kuHL";
+  document.getElementsByTagName("head")[0].appendChild(script);
+
+  const recaptchaInterval = setInterval(function () {
+    if (!window.grecaptcha || !window.grecaptcha.execute) {
+      return;
+    }
+    clearInterval(recaptchaInterval);
+    console.log("Recaptcha Initialised");
+  }, 100);
+})();
+
+const getRecaptchaToken = (action) =>
+  new Promise((resolve, reject) =>
+    window.grecaptcha.ready(() =>
+      window.grecaptcha
+        .execute("6Ldwb4InAAAAAP8Z6U1JnGq3WQ9pguOtWGh3kuHL", { action })
+        .then(resolve)
+        .catch(reject),
+    ),
+  );
+
 const openSubscribeModalButtons = document.getElementsByClassName(
   "open-subscribe-modal-button",
 );
@@ -77,7 +106,31 @@ class Subscribe {
         throw new Error("Invalid email!");
       }
 
-      // @todo Call API
+      const { res, err } = await sf
+        .useOnce(
+          "https://prod-api.gcp-cr.thepmftool.com/v1/landing/contact-form/submit",
+        )
+        .POST()
+        .useHeader({
+          "x-recaptcha-token": await getRecaptchaToken("contactUs"),
+        })
+        .bodyJSON({
+          name: "blog.JJSS.quest",
+          email,
+          message: "blog subscription",
+        })
+        .runJSON();
+
+      if (err) {
+        throw err;
+      }
+      if (!res.ok) {
+        throw new Error(JSON.stringify(res, null, 2));
+      }
+
+      if (res.status === 201) {
+        alert("Subscribed, you will get new posts in your inbox directly!");
+      }
     } catch (error) {
       alert(`Failed to subscribe: ${error}`);
       console.error(error);
