09cd52cc219f2669ee16263a1c0e50e7e6f59f59
Jaime <jaimeloeuf@gmail.com>
2024-06-22 00:22:27 +0800

    Experiment with generating generators.

---
+++ tool/src/genTemplateGenerators.ts
@@ -0,0 +1,36 @@
+import { readdir } from "fs/promises";
+import { templateDirPath } from "./utils/dirPaths";
+
+/**
+ *
+ */
+export async function genTemplateGenerators() {
+  const templateDirItems = await readdir(templateDirPath, { recursive: true });
+
+  const templates = templateDirItems.filter((template) =>
+    template.endsWith(".html"),
+  );
+
+  for (const template of templates) {
+    const templateFileName = template.split("/").pop();
+    if (templateFileName === undefined) {
+      throw new Error("Unable to get template's file name");
+    }
+
+    const templateFileNameWithoutHtml = templateFileName.slice(
+      0,
+      templateFileName.length - ".html".length,
+    );
+
+    // Capitalise this for camel case with the 'generate' prefix
+    const generatedFunctionName =
+      "generated" +
+      templateFileNameWithoutHtml[0]?.toUpperCase() +
+      templateFileNameWithoutHtml.slice(1);
+
+    let generatedCode = `export const ${generatedFunctionName} = () =>
+rfs("${template}")`;
+  }
+}
+
+genTemplateGenerators();
