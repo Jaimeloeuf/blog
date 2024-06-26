2fc50aabfa6dca635e798dfe1656248ee7ddc147
Jaime <jaimeloeuf@gmail.com>
2024-06-26 20:22:28 +0800

    Refactor genTemplateCreator out.

---
+++ tool/src/codegen/genTemplateCreator.ts
@@ -0,0 +1,41 @@
+/**
+ * Generate a single 'template creator' function code using template file path
+ */
+export function genTemplateCreator(templatePath: string) {
+  const templatePathComponents = templatePath.split("/");
+
+  // Generate template type by camel casing the path components
+  const templateType =
+    (templatePathComponents[0] ?? "") +
+    templatePathComponents
+      .slice(1, templatePathComponents.length - 1)
+      .map(
+        (pathComponent) =>
+          pathComponent[0]?.toUpperCase() + pathComponent.slice(1),
+      );
+  if (templateType === undefined) {
+    throw new Error("Unable to get template's file type");
+  }
+
+  const templateFileName =
+    templatePathComponents[templatePathComponents.length - 1];
+  if (templateFileName === undefined) {
+    throw new Error("Unable to get template's file name");
+  }
+
+  const templateFileNameWithoutHtml = templateFileName.slice(
+    0,
+    templateFileName.length - ".html".length,
+  );
+
+  // Capitalise this for camel case with the 'create' prefix
+  const functionName =
+    "create" +
+    templateFileNameWithoutHtml[0]?.toUpperCase() +
+    templateFileNameWithoutHtml.slice(1) +
+    templateType[0]?.toUpperCase() +
+    templateType.slice(1);
+
+  return `export const ${functionName} = () =>
+rfs("${templatePath}")`;
+}
--- tool/src/codegen/genTemplateCreators.ts
+++ tool/src/codegen/genTemplateCreators.ts
@@ -2,48 +2,7 @@ import path from "path";
 import { readdir, writeFile } from "fs/promises";
 import { templateDirPath, generatedSrcDirPath } from "../utils/dirPaths";
 import { logger } from "../../shared/logger";
-
-/**
- * Generate a single 'template creator' function code using template file path
- */
-function genTemplateCreator(templatePath: string) {
-  const templatePathComponents = templatePath.split("/");
-
-  // Generate template type by camel casing the path components
-  const templateType =
-    (templatePathComponents[0] ?? "") +
-    templatePathComponents
-      .slice(1, templatePathComponents.length - 1)
-      .map(
-        (pathComponent) =>
-          pathComponent[0]?.toUpperCase() + pathComponent.slice(1),
-      );
-  if (templateType === undefined) {
-    throw new Error("Unable to get template's file type");
-  }
-
-  const templateFileName =
-    templatePathComponents[templatePathComponents.length - 1];
-  if (templateFileName === undefined) {
-    throw new Error("Unable to get template's file name");
-  }
-
-  const templateFileNameWithoutHtml = templateFileName.slice(
-    0,
-    templateFileName.length - ".html".length,
-  );
-
-  // Capitalise this for camel case with the 'create' prefix
-  const functionName =
-    "create" +
-    templateFileNameWithoutHtml[0]?.toUpperCase() +
-    templateFileNameWithoutHtml.slice(1) +
-    templateType[0]?.toUpperCase() +
-    templateType.slice(1);
-
-  return `export const ${functionName} = () =>
-rfs("${templatePath}")`;
-}
+import { genTemplateCreator } from "./genTemplateCreator";
 
 /**
  * Generate 'template creator' functions for all templates
