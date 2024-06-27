f9de94f7b80b4e5dae1663900626b2c583675de6
Jaime <jaimeloeuf@gmail.com>
2024-06-27 21:17:52 +0800

    Add generated code hash to generated code notice.

--- tool/src/codegen/genGeneratedCodeFile.ts
+++ tool/src/codegen/genGeneratedCodeFile.ts
@@ -1,10 +1,16 @@
+import { createHash } from "node:crypto";
 import { genGeneratedNotice } from "./genGeneratedNotice";
 
 export function genGeneratedCodeFile(
   generator: Function,
   generatedCode: string,
 ) {
-  const notice = genGeneratedNotice(generator);
+  const codeHash = createHash("sha256")
+    .update(generatedCode)
+    .digest()
+    .toString("hex");
+
+  const notice = genGeneratedNotice(generator, codeHash);
 
   return notice + generatedCode;
 }
