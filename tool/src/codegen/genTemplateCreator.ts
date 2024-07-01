2297a4e817843f2629443e8e4116137d09c1f7c3
Jaime <jaimeloeuf@gmail.com>
2024-07-02 00:29:55 +0800

    Update codegen to support types in template.

--- tool/src/codegen/genTemplateCreator.ts
+++ tool/src/codegen/genTemplateCreator.ts
@@ -18,13 +18,28 @@ export async function genTemplateCreator(templatePath: string) {
   ).map((templateVariable) => templateVariable.slice(2, -1));
 
   // Create destructured object params if there are template variables
-  const params =
-    templateVariables.length === 0
-      ? ""
-      : `{${templateVariables.join()}}: {${templateVariables.map((variable) => variable + ": string|number;").join("")}}`;
+  const params = templateVariablesParser(templateVariables);
 
   const JSDoc = `/**\n * Template: src/template/${templatePath}\n */\n`;
   const code = `export const ${functionName} = (${params}) => \`${file}\``;
 
   return JSDoc + code;
+}
+
+/**
+ * Create destructured object params for each variable found in template
+ */
+function templateVariablesParser(templateVariables: Array<string>) {
+  if (templateVariables.length === 0) {
+    return "";
+  }
+
+  const paramType = templateVariables
+    .map((variable) => {
+      const [variableName, type = "string"] = variable.split(":");
+      return `${variableName}: ${type};`;
+    })
+    .join("");
+
+  return `{${templateVariables.join()}}: {${paramType}}`;
 }
