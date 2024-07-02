a5c23b802ac2b654a866a061480a1198640a6602
Jaime <jaimeloeuf@gmail.com>
2024-07-03 00:20:48 +0800

    Refactor code out.

--- tool/src/codegen/genTemplateCreator.ts
+++ tool/src/codegen/genTemplateCreator.ts
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { readFile } from "fs/promises";
 import { templateDirPath } from "../utils/dirPaths";
 import { genTemplateCreatorFunctionName } from "./genTemplateCreatorFunctionName";
-import { logger } from "../../shared/logger";
+import { parseTemplateForTemplateAndVariables } from "./parseTemplateForTemplateAndVariables";
 
 /**
  * Generate a single 'template creator' function code using template file path
@@ -14,54 +14,14 @@ export async function genTemplateCreator(templatePath: string) {
     encoding: "utf8",
   });
 
-  const templateVariables = new Set<string>();
-  const templateVariableParamTypes = new Set<string>();
-
-  // Strip variable types from file to use it as JS string template, and also
-  // saves the variables and types to generate the parameter types of the
-  // creator functions later.
-  const newFileWithoutTypes = file.replaceAll(
-    /\$\{([^}]+)\}/g,
-    (rawVariableString) => {
-      // Slice out the surrounding ${ }
-      const variable = rawVariableString.slice(2, -1);
-
-      // Get variable type from variable definition if any, else default to string
-      // type since anything that is string or have .toString() is supported
-      const [variableName = "", variableType = "string"] = variable
-        .replaceAll(" ", "")
-        .split(":");
-
-      const validTemplateVariableTypes = ["string", "number", "boolean"];
-      if (!validTemplateVariableTypes.includes(variableType)) {
-        logger.info(
-          genTemplateCreator.name,
-          `Found invalid template variable type: ${variableType}`,
-        );
-        logger.info(
-          genTemplateCreator.name,
-          `Template variable: '${variable}'`,
-        );
-        logger.info(genTemplateCreator.name, `Found in: ${templatePath}`);
-        throw new Error(`Invalid template variable type`);
-      }
-
-      // Save the template variable found, using a Set to guarantee uniqueness
-      templateVariables.add(variableName);
-
-      // Create the creator function parameter type
-      templateVariableParamTypes.add(`${variableName}: ${variableType};`);
-
-      // Add back ${ } but without variable type to use in JS string template
-      return `\${${variableName}}`;
-    },
-  );
+  const { templateVariables, templateVariableParamTypes, newFileWithoutTypes } =
+    parseTemplateForTemplateAndVariables(file, templatePath);
 
   // Create destructured object params if there are template variables
   const params =
-    templateVariables.size === 0
+    templateVariables.length === 0
       ? ""
-      : `{${Array.from(templateVariables).join()}}: {${Array.from(templateVariableParamTypes).join("")}}`;
+      : `{${templateVariables.join()}}: {${templateVariableParamTypes.join("")}}`;
 
   const JSDoc = `/**\n * Template: src/template/${templatePath}\n */\n`;
   const code = `export const ${functionName} = (${params}) => \`${newFileWithoutTypes}\``;
---
+++ tool/src/codegen/parseTemplateForTemplateAndVariables.ts
@@ -0,0 +1,57 @@
+import { logger } from "../../shared/logger";
+import { genTemplateCreator } from "./genTemplateCreator";
+
+const validTemplateVariableTypes = ["string", "number", "boolean"];
+
+/**
+ * Parses the template to strip out any variable types and to extract out the
+ * variables with their type information for generating the function parameters.
+ */
+export function parseTemplateForTemplateAndVariables(
+  file: string,
+  templatePath: string,
+) {
+  const templateVariables = new Set<string>();
+  const templateVariableParamTypes = new Set<string>();
+
+  // Strip variable types from file to use it as JS string template, and also
+  // saves the variables and types to generate the parameter types of the
+  // creator functions later.
+  const newFileWithoutTypes = file.replaceAll(
+    /\$\{([^}]+)\}/g,
+    (rawVariableString) => {
+      // Slice out the surrounding ${ }
+      const variable = rawVariableString.slice(2, -1);
+
+      // Get variable type from variable definition if any, else default to
+      // string since anything that is string or have .toString() is supported
+      const [variableName = "", variableType = "string"] = variable
+        .replaceAll(" ", "")
+        .split(":");
+
+      if (!validTemplateVariableTypes.includes(variableType)) {
+        logger.info(
+          genTemplateCreator.name,
+          `Found invalid template variable type: ${rawVariableString}`,
+        );
+        logger.info(genTemplateCreator.name, `Found in: ${templatePath}`);
+        throw new Error(`Invalid template variable type`);
+      }
+
+      // Save the template variable found, using a Set to guarantee uniqueness
+      templateVariables.add(variableName);
+
+      // Create the creator function parameter type
+      templateVariableParamTypes.add(`${variableName}: ${variableType};`);
+
+      // Add back ${ } but without variable type to use in JS string template
+      return `\${${variableName}}`;
+    },
+  );
+
+  return {
+    templateVariables: Array.from(templateVariables),
+    templateVariableParamTypes: Array.from(templateVariableParamTypes),
+    newFileWithoutTypes,
+  };
+}
